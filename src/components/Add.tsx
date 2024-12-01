3c5e1047ee0e673694021943ba7ec8bedf621fc6
Luiz Moragas <luiz.moragas@ecollect.net>
2024-12-01 23:09:13 +0100

    front end v0 finished

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -5,15 +5,27 @@ import { useState } from "react";
 const Add = () => {
     const [amount, setAmount] = useState(1);
 
+    // TEMPORARY
+    const stock = 2;
+
+    const handleAmount = (type: 'i' | 'd' ) => {
+        if(type==='d' && amount > 1){
+            setAmount((prev) => prev -1);
+        }
+        if(type==='i' && amount < stock){
+            setAmount((prev) => prev + 1);
+        }
+    };
+
     return (
         <div className="flex flex-col gap-4">
             <h4 className="text-text font-medium">Choose a Quantity</h4>
             <div className="flex justify-between">
                 <div className="flex items-center gap-4">
                     <div className="bg-text py-2 px-4 rounded-3xl flex items-center justify-between w-32">
-                        <button className="cursor-pointer text-xl">-</button>
+                        <button className="cursor-pointer text-xl" onClick={()=>handleAmount('d')}>-</button>
                         {amount}
-                        <button className="cursor-pointer text-xl">+</button>
+                        <button className="cursor-pointer text-xl" onClick={()=>handleAmount('i')}>+</button>
                     </div>
                     <div className="text-text text-xs">Only <span className="text-disabledOrange">2 items</span> left! <br /> {"Don't"} miss it</div>
                 </div>
